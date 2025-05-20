7f935875ee0c062d856253358e977dc66bda4ac8
mrpong22 <phong.dx@st.buv.edu.vn>
2025-05-20 13:38:17 +0700

    New animations

--- Assets/Boss/Scripts/BossController.cs
+++ Assets/Boss/Scripts/BossController.cs
@@ -121,6 +121,10 @@ public class BossController : MonoBehaviour
             {
                 yield return null;
             }
+            if (player != null && !player.IsDodging())
+            {
+                player.TakeDamage(attackPower);
+            }
             turnManager.EndEnemyTurn();
         }
         else
